2fe7f99bb3f51ccc2118fb19d9f1a8032e8fa1c4
Ayush Goel <ayush.g@directi.com>
2013-05-19 17:09:05 +0530

    Remove repetetive call when category changed. When category changed, layout subviews take cares of creating pages.

--- EmojiKeyBoard/EmojiKeyBoardView.m
+++ EmojiKeyBoard/EmojiKeyBoardView.m
@@ -20,6 +20,7 @@
 static NSString *const segmentRecentName = @"Recent";
 NSString *const RecentUsedEmojiCharactersKey = @"RecentUsedEmojiCharactersKey";
 
+// fixme: setting numberOfPages on pagecontrol calls setNeedsDisplay and thus calls layoutSubviews
 
 @interface EmojiKeyBoardView () <UIScrollViewDelegate, EmojiPageViewDelegate>
 
@@ -183,12 +184,11 @@ NSString *const RecentUsedEmojiCharactersKey = @"RecentUsedEmojiCharactersKey";
   NSLog(@"Category changed %d", sender.selectedSegmentIndex);
   NSArray *categoryList = @[segmentRecentName, @"People", @"Objects", @"Nature", @"Places", @"Symbols"];
   self.category = categoryList[sender.selectedSegmentIndex];
+  [self.scrollView.subviews makeObjectsPerformSelector:@selector(removeFromSuperview)];
   NSUInteger numberOfPages = [self numberOfPagesForCategory:self.category inFrameSize:self.scrollView.bounds.size];
   self.pageControl.currentPage = 0;
   self.pageControl.numberOfPages = numberOfPages;
-  [self.scrollView.subviews makeObjectsPerformSelector:@selector(removeFromSuperview)];
-  self.scrollView.contentOffset = CGPointMake(0, 0);
-  [self createPagesWithNumberOfPages:numberOfPages setCurrentPage:0];
+  // layout subviews called here as pagecontrol number of pages changes
 }
 
 - (void)pageControlTouched:(DDPageControl *)sender {
