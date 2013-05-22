e5c975bd841dabda3f3238ba0dddd8aa7d5a8936
Ayush Goel <ayush.g@directi.com>
2013-05-22 15:22:03 +0530

    Add documentation to .h files

--- EmojiKeyBoard/EmojiKeyBoardView.h
+++ EmojiKeyBoard/EmojiKeyBoardView.h
@@ -5,6 +5,8 @@
 //  Created by Ayush on 09/05/13.
 //  Copyright (c) 2013 Ayush. All rights reserved.
 //
+// Set as inputView to textfields, this view class gives an
+// interface to the user to enter emoji characters.
 
 #import <UIKit/UIKit.h>
 
