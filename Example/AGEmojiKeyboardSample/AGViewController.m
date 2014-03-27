50f7d886853d0f00fd043d4019c3f693a8e648f8
Ayush Goel <ayush.g@directi.com>
2014-03-27 13:55:45 +0530

    Modify the sample VC to preset the keyboard

--- Example/AGEmojiKeyboardSample/AGViewController.m
+++ Example/AGEmojiKeyboardSample/AGViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "AGViewController.h"
+#import <AGEmojiKeyboard/AGEmojiKeyboardView.h>
 
-@interface AGViewController ()
+@interface AGViewController () <AGEmojiKeyboardViewDelegate, AGEmojiKeyboardViewDataSource>
+
+@property (nonatomic) UITextView *textView;
 
 @end
 
@@ -16,14 +19,64 @@
 
 - (void)viewDidLoad
 {
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+  [super viewDidLoad];
+
+
+  self.textView = [[UITextView alloc] initWithFrame:self.view.frame];
+  self.textView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+  AGEmojiKeyboardView *emojiKeyboardView = [[AGEmojiKeyboardView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 216) dataSource:self];
+  emojiKeyboardView.autoresizingMask = UIViewAutoresizingFlexibleHeight;
+  emojiKeyboardView.delegate = self;
+  [self.view addSubview:self.textView];
+  self.textView.inputView = emojiKeyboardView;
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)emojiKeyBoardView:(AGEmojiKeyboardView *)emojiKeyBoardView didUseEmoji:(NSString *)emoji {
+  self.textView.text = [self.textView.text stringByAppendingString:emoji];
+}
+
+- (void)emojiKeyBoardViewDidPressBackSpace:(AGEmojiKeyboardView *)emojiKeyBoardView {
+
+}
+
+- (UIColor *)randomColor {
+  return [UIColor colorWithRed:drand48()
+                         green:drand48()
+                          blue:drand48()
+                         alpha:drand48()];
+}
+
+- (UIImage *)randomImage {
+  CGSize size = CGSizeMake(30, 10);
+  UIGraphicsBeginImageContextWithOptions(size , NO, 0);
+
+  CGContextRef context = UIGraphicsGetCurrentContext();
+  UIColor *fillColor = [self randomColor];
+  CGContextSetFillColorWithColor(context, [fillColor CGColor]);
+  CGRect rect = CGRectMake(0, 0, size.width, size.height);
+  CGContextFillRect(context, rect);
+
+  fillColor = [self randomColor];
+  CGContextSetFillColorWithColor(context, [fillColor CGColor]);
+  CGFloat xxx = 3;
+  rect = CGRectMake(xxx, xxx, size.width - 2 * xxx, size.height - 2 * xxx);
+  CGContextFillRect(context, rect);
+
+  UIImage *img = UIGraphicsGetImageFromCurrentImageContext();
+  UIGraphicsEndImageContext();
+  return img;
+}
+
+- (UIImage *)emojiKeyboardView:(AGEmojiKeyboardView *)emojiKeyboardView imageForSelectedCategory:(AGEmojiKeyboardViewCategoryImage)category {
+  UIImage *img = [self randomImage];
+  [img imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+  return img;
+}
+
+- (UIImage *)emojiKeyboardView:(AGEmojiKeyboardView *)emojiKeyboardView imageForNonSelectedCategory:(AGEmojiKeyboardViewCategoryImage)category {
+  UIImage *img = [self randomImage];
+  [img imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+  return img;
 }
 
 @end
